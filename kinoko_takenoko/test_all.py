98825fa55616b119739b53a4da2956d5f3999901
Yusuke Suzuki <supo.nyiyan437@gmail.com>
2019-02-26 19:12:50 +0900

    fix test_all

--- kinoko_takenoko/test_all.py
+++ kinoko_takenoko/test_all.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import roc_curve
-from sklearn.metrics import auc
 import matplotlib.pyplot as plt
 import argparse
 import os
